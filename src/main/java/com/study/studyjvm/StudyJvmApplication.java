b5b183767e81c4dcc678536e269036e59ce63753
nacycher <nacycher@163.com>
2025-04-09 17:07:29 +0800

    init project

---
+++ src/main/java/com/study/studyjvm/StudyJvmApplication.java
@@ -0,0 +1,13 @@
+package com.study.studyjvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJvmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/studyjvm/StudyJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studyjvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
