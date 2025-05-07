77ecb5f1831f65f08ed44c60e85acb00521c92ae
nacycher <nacycher@163.com>
2025-05-07 15:12:55 +0800

    [add]07-class文件结构分析

---
+++ src/main/java/com/study/studyjvm/java1/Demo.java
@@ -0,0 +1,14 @@
+package com.study.studyjvm.java1;
+
+/**
+ * 演示java class文件的结构
+ */
+public class Demo {
+    private int num = 1;
+
+    public int add() {
+        num += 2;
+        return num;
+
+    }
+}
